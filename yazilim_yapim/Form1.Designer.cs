1779e14b67d54f5ca982efa3335469db886bbf7c
Cihat Şenel <cihat-cy@hotmail.com>
2021-04-25 18:38:20 +0300

    login ekranı ve database oluşturuldu

---
+++ yazilim_yapim/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace yazilim_yapim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GİRİŞ YAP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Parola:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 113);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Admin Giriş";
+            this.radioButton1.UseVisualStyleBackColor = true;
+          
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(99, 113);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Alıcı Giriş";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(167, 113);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(77, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Satıcı  Giriş";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 183);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "KULLANICI GİRİŞ EKRANI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ yazilim_yapim/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace yazilim_yapim
+{
+    public partial class Form1 : Form
+    {   //Veri tabanı bağlama
+        SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-0GKB0TH;Initial Catalog=borsa;Integrated Security=True");
+        //----------------------------------------------------------------
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            baglan.Open();
+            SqlCommand command = new SqlCommand("Select * from kullanicii", baglan);
+            SqlDataReader reader = command.ExecuteReader();
+            
+            string username = textBox1.Text;
+            string pass = textBox2.Text;
+ 
+                if (radioButton1.Checked) // admin giriş
+
+                {
+                if (username != "" && pass != "")
+                {
+
+                    while (reader.Read())
+                    {
+                        string kullanici_ad = reader["kullanici_ad"].ToString();
+                        string kullanici_password = reader["kullanici_password"].ToString();
+                        string kullanici_rol = reader["kullanici_rol"].ToString();
+                        if (username == kullanici_ad && pass == kullanici_password && kullanici_rol=="admin") // veriler veri tabınından çekilecek.
+                        {
+                            MessageBox.Show("Admin Paneline Yöneliyorsunuz...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Yanlış Kullanıcı adı ya da şifre admin şifresi ile uyuşmuyor");
+
+                        }
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı ve ya parola boş bırakılamaz.....");
+                }
+                }
+                else if (radioButton2.Checked) // Alıcı giriş
+                {
+                if (username != "" && pass != "")
+                {
+                    while (reader.Read())
+                    {
+                        string kullanici_ad = reader["kullanici_ad"].ToString();
+                        string kullanici_password = reader["kullanici_password"].ToString();
+                        string kullanici_rol = reader["kullanici_rol"].ToString();
+
+                        if (username == kullanici_ad && pass == kullanici_password && kullanici_rol == "alici") // veriler veri tabınından çekilecek.
+                        {
+                            MessageBox.Show("Alıcı Paneline Yöneliyorsunuz...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Yanlış Kullanıcı adı ya da şifre.....");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı ve ya parola boş bırakılamaz.....");
+                }
+                }
+                else if (radioButton3.Checked) // Satici giriş
+                {
+                if (username != "" && pass != "")
+                {
+                    while (reader.Read())
+                    {
+                        string kullanici_ad = reader["kullanici_ad"].ToString();
+                        string kullanici_password = reader["kullanici_password"].ToString();
+                        string kullanici_rol = reader["kullanici_rol"].ToString();
+                        if (username == kullanici_ad && pass == kullanici_password && kullanici_rol == "satici") // veriler veri tabınından çekilecek.
+                        {
+                            MessageBox.Show("Satıcı Yöneliyorsunuz...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Yanlış Kullanıcı adı ya da şifre.....");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı ve ya parola boş bırakılamaz.....");
+                }
+                }
+               
+            
+            baglan.Close();
+        }
+
+       
+    }
+}
